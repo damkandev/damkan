76f9aaa91a0c79c88c9a7aa303d5a4b6e0fb0e87
Damián Panes <damian@granmenu.me>
2025-07-10 08:17:59 -0400

    feat: add new SVG logos for Python, React, and Rust; remove unused global styles and components; restructure layout with Astro

--- src/components/CardVentana.jsx
+++
@@ -1,5 +0,0 @@
-export default function CardVentana({ children }) {
-  return (
-    <div className="border border-borde-gris px-4 py-9 w-full">{children}</div>
-  );
-}
--- src/components/Toast.jsx
+++
@@ -1,7 +0,0 @@
-export default function Toast() {
-  return (
-    <div className="flex w-full bg-gradient-to-r from-naranja-gradient-inicial to-naranja-gradient-final md:w-[50rem] my-4 p-4 text-[#2C221D] font-JetBrains-Bold text-center justify-center text-sm md:text-lg">
-      <p>React, NextJS, GSAP, PostgreSQL, Python y Flask.</p>
-    </div>
-  );
-}
